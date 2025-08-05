fd2db1342d350ab92b92b50f01cb1a2374100f7c
manjur0 <sfmanjur21@gmail.com>
2025-08-05 20:53:56 +0600

    Add event listener to ring buttons for color selection and image update

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const ringButton = document.querySelectorAll(".ring-button");
 for (let i = 0; i < ringButton.length; i++) {
   const ringBtn = ringButton[i];
   ringBtn.addEventListener("click", function (event) {
+    // get button id for color
+    const color = event.target.id;
+    console.log(color);
     // cheeck previuse border
     for (let j = 0; j < ringButton.length; j++) {
       ringButton[j].classList.remove("border-purple-700");
@@ -9,5 +12,9 @@ for (let i = 0; i < ringButton.length; i++) {
 
     //add color
     event.target.classList.add("border-purple-700");
+
+    // get product image
+    const productImage = document.getElementById("product-image");
+    productImage.src = "./images/gray.png";
   });
 }
