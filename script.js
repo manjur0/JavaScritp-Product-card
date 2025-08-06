096dffb0ee13b35feea8fcbe63b04028e0dffae4
manjur0 <sfmanjur21@gmail.com>
2025-08-06 19:48:25 +0600

    Refactor event listener for ring buttons to improve readability and fix string concatenation

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const ringButton = document.querySelectorAll(".ring-button");
+
 for (let i = 0; i < ringButton.length; i++) {
   const ringBtn = ringButton[i];
   ringBtn.addEventListener("click", function (event) {
     // get button id for color
     const color = event.target.id.replace("-color", "");
-      console.log(color);
-      
+    console.log(color);
+
     // cheeck previuse border
     for (let j = 0; j < ringButton.length; j++) {
       ringButton[j].classList.remove("border-purple-700");
@@ -16,6 +17,6 @@ for (let i = 0; i < ringButton.length; i++) {
 
     // get product image
     const productImage = document.getElementById("product-image");
-    productImage.src = "./images/" + color + ".png";
+    productImage.src = `./images/${color}.png`;
   });
 }
