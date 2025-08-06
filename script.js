0929dcbdda2ee09c65983ee417defbbccebf63bc
manjur0 <sfmanjur21@gmail.com>
2025-08-06 20:07:21 +0600

    Add size selection functionality with visual feedback for product sizes

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ for (let i = 0; i < ringButton.length; i++) {
     productImage.src = `./images/${color}.png`;
   });
 }
+
+function selectSize(size) {
+  const sizes = ["S", "M", "L", "XL"];
+  for (let i = 0; i < sizes.length; i++) {
+    const button = document.getElementById("size-" + sizes[i]);
+    const element = sizes[i];
+    if (size === element) {
+      button.classList.add("border-purple-700");
+    } else {
+      button.classList.remove("border-purple-700");
+    }
+  }
+}
